15809858d3a076e01df15b748e9f6539d252a802
lucasnike <lucas4cristal@gmail.com>
2019-10-31 23:43:26 -0300

    Adicionei a classe Board e a Piece

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,5 +1,6 @@
 package aplication;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
@@ -7,6 +8,8 @@ public class Program {
 	public static void main(String[] args) {
 		Position pos = new Position(1, 5);
 		System.out.println(pos);
+		
+		Board board = new Board(8, 8);
 	}
 
 }

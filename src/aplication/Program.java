7e96cf7c37e51f8fa54e30be85f94a55e538d7e2
lucasnike <lucas4cristal@gmail.com>
2019-10-31 23:27:52 -0300

    Classe Position criada

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,10 +1,12 @@
 package aplication;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!!");
+		Position pos = new Position(1, 5);
+		System.out.println(pos);
 	}
 
 }
